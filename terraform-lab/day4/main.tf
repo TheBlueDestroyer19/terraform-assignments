70abb19c3266034cffd89ab1ea8aa94e63b1b07c
TheBlueDestroyer19 <salilnphanse@gmail.com>
2026-03-23 04:39:28 +0000

    Assignment 4 - Terraform modules

---
+++ terraform-lab/day4/main.tf
@@ -0,0 +1,36 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "docker" {}
+
+# First container
+module "nginx1" {
+  source = "./modules/nginx_container"
+
+  container_name = "nginx-1"
+  container_port = 8081
+  image_name     = "nginx:latest"
+}
+
+# Second container
+module "nginx2" {
+  source = "./modules/nginx_container"
+
+  container_name = "nginx-2"
+  container_port = 8082
+  image_name     = "nginx:latest"
+}
+
+# Output
+output "urls" {
+  value = [
+    "http://localhost:8081",
+    "http://localhost:8082"
+  ]
+}
